1e0d4073a94b6b195c987ebbad69e293e606fb6d
YB_KIM <52683010+yeongbin123@users.noreply.github.com>
2020-05-08 15:18:11 +0900

    Revise code

--- commentRemover.py
+++ commentRemover.py
@@ -1,3 +1,4 @@
+# from pydriller import *
 from git import *
 import os
 import shutil
@@ -5,6 +6,7 @@ import stat
 import csv
 import re
 import subprocess
+import SetData as sd
 # -------------------------------------------------------------------------------------------------------
 def remove_readonly(func, path, excinfo):
     os.chmod(path, stat.S_IWRITE)
@@ -16,51 +18,9 @@ def remove_dir_tree(remove_dir):
     except(PermissionError) as e:
         print("[Delete Error] %s - %s." % (e.filename,e.strerror))
 # -------------------------------------------------------------------------------------------------------
-# Make a clone data folder
-try:
-    # If file is not exist 
-    if not(os.path.isdir('clone_data')): 
-        os.makedirs(os.path.join('clone_data'))
-except OSError as e:
-    if e.errno != errno.EEXIST:
-        # Ignore exception
-        print("Alerady exist") 
-
-"""
-    이 부분을 바꿔야 함. 주석을 제거하는 기준은 MappingCommitFiles가 아닌 csv file임 - 일단 보류
-
-    1. csv file에서 +로 시작하는 부분을 따로 추출해야 함 (이게 곧 Mapping folder에 있는 file이기 때문)
-    2. 이때 파일을 바로 만드는게 아니라 comment인지 확인 후 만드는게 더 효율적으로 보임
-
-"""
-
-# -------------------------------------------------------------------------------------------------------
-path_dir = "C:/Lab/EJTool/Ngram/data/MappingCommitFiles"
-        
-file_list = os.listdir(path_dir) # Save file list in MappingCommitFiles
-
-
-filename = input("Input a filename that include url information : ")
+filename, file_hash, file_dict, file_list = sd.InitData()
 
 cnt = 0
-file_dict = dict()
-file_hash = []
-
-csv_f = open('sample.csv','r') # Open csv file
-
-rdr = csv.reader(csv_f) # Get text 
-
-for line in rdr:
-    # It is a variable that used to ignore the first line of csv file
-    if cnt > 0: 
-        # Separate file extension    
-        re_line = line[2].split(".") 
-        file_hash.append(re_line[0])
-        # Hash value and path mapping
-        file_dict[line[2]] = line[5] 
-    cnt += 1
-
-cnt = 0 # Reuse
 
 # File is cloned based on url.
 with open(filename,'r') as txtinput:
